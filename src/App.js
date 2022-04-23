532e53d2aa4d27ecd3cf36c3df5fd207ccf8b49e
hamza <hamza.chaouai@gmail.com>
2022-04-24 01:02:31 +0200

    todolist done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+//importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import Header from "./components/Header";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filtredTodos, setFiltredTodos] = useState([]);
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFiltredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFiltredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFiltredTodos(todos);
+        break;
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Form
+        todos={todos}
+        inputText={inputText}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList setTodos={setTodos} todos={todos} filtredTodos={filtredTodos} />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <ul id="nav">
+        <li class="nav-link">
+          <h1 data-name="Hamza">Hamza</h1>
+        </li>
+        <li class="nav-link">
+          <h1 data-name="CHAOUAI">CHAOUAI</h1>
+        </li>
+        <li class="nav-link">
+          <h1 data-name="TODO">TODO</h1>
+        </li>
+        <li class="nav-link">
+          <h1 data-name="LIST">LIST</h1>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
